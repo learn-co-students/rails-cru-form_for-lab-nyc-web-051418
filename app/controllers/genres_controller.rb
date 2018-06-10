542b7fe7e7df0e270de3a8746e0b8832d8e6c343
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-10 11:16:36 -0400

    Solved lab

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,30 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def new; end
+
+  def create
+    @genre = Genre.create(strong_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(strong_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def strong_params(*args)
+    params.require(:genre).permit(*args)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artist1 = Artist.create(name: "Miguel Bose", bio: "Born in 1987")
+artist2 = Artist.create(name: "Isabel Pantoja", bio: "Born in 1945")
+genre1 = Genre.create(name: "Pop")
+genre2 = Genre.create(name: "Traditional")
+Song.create(name: "Como una ola", artist_id: artist1[:id] , genre_id: genre1[:id])
+Song.create(name: "Mola mazo", artist_id: artist2[:id] , genre_id: genre2[:id])
