fd44df8bb21730c61132e9aed7f3798b8f5f9bbf
github username <github email address>
2018-06-11 17:46:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Artist.create(name: "Kanye West", bio: "Im god")
+a2 = Artist.create(name: "Lil uzi vert", bio: "ya!")
+a3 = Artist.create(name: "Taylor Swift", bio: "I love the guitar")
+
+g1 = Genre.create(name: "Rap")
+g2 = Genre.create(name: "Country")
+
+drive = Song.create(name: "I can drive", artist: a2, genre: g1)
+heart = Song.create(name: "Heartless", artist: a1, genre: g1)
+tomg = Song.create(name: "Teardrops on my guitar", artist: a3, genre: g2)
